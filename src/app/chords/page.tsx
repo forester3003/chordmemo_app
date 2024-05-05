be4795c177d0b6d9e2f80d82370b89d602823e9d
forester3003 <palao18e@gmail.com>
2024-05-05 05:41:49 +0000

    [update]Fretboard.js first render
    Open C Chord rendering by fretboard.js

--- src/app/chords/page.tsx
+++ src/app/chords/page.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import Guitar from '../components/guitar/Guitar'
+'use client'
+
+import React, { useEffect, useRef } from 'react'
+import { Fretboard } from '@moonwave99/fretboard.js'
 
 export default function Chord() {
+  const fretboardRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    // レンダリング
+    if (fretboardRef.current && !fretboardRef.current.innerHTML) {
+      // fretboardRef.current.innerHTML = ''; // すでに要素が存在する場合はクリアする
+      const fretboard = new Fretboard({
+        el: fretboardRef.current,
+      });
+      fretboard.setDots([
+        {
+          string: 5,
+          fret: 3,
+        },
+        {
+          string: 4,
+          fret: 2,
+        },
+        {
+          string: 2,
+          fret: 1,
+        },
+      ]);
+      fretboard.render();
+      console.log("rendered");
+    }
+  }, [fretboardRef.current]); // fretboardRef.current が変更されるたびに再レンダリングをトリガー
+
   return (
     <div className='h-screen'>
       <div className='text-4xl text-center'>Guitar</div>
-      <div className='flex justify-center'>
-        <Guitar />
-      </div>
+      <figure ref={fretboardRef}></figure>
     </div>
   )
 }
